d3c3d11345b11f0bf7940534a29bcc994a3066bf
Felipe Melo <ftiagomelo89@gmail.com>
2021-01-27 00:33:32 -0300

    Projeto criado

---
+++ src/test/java/com/felipemelo/pandemicsystem/PandemicCombatAidSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemelo.pandemicsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicCombatAidSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
